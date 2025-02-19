7665a8df81b8255b9386bfdf85652d0b7d8a4a49
¨ABDERAHMEN609¨ <abderahmenyoussfi22@gmail.com>
2025-02-19 11:34:59 +0100

    node mailer

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+var fs = require("fs")
+var http = require("http");
+http.createServer(function (request, response) {
+  
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    
+    
+
+    response.end('Hello World\n');
+    
+ }).listen(8081);
+ var data = fs.readFileSync("hello.txt")
+    console.log(data.toString());
+
+ console.log('Server running at http://127.0.0.1:8081/');
+
---
+++ nodemailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: process.env.USER, // sender address
+    to: process.env.CLIENT, // list of receivers
+    subject: "hgjhfn", // Subject line
+    text: "rhazem bou", // plain text body
+    html: "<b>rhazem bou</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
